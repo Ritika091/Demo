e8c4c109ad714e651ec0d2f79e7561b1c8089801
Swarga-codes <sd3526@srmist.edu.in>
2024-04-04 17:29:33 +0530

    new feature

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
+console.log('new feature')
+
+
+
